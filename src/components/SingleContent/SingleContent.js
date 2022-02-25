6589fc50ca86ae18c31faa255bb3b007a2dc8ad5
Naman9761 <Namanjoc9761@gmail.com>
2022-02-26 02:29:20 +0530

    Second Commit

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,6 +1,9 @@
 import { Badge } from "@material-ui/core";
-import { img_300, unavailable } from "../config/config";
+import { img_300, unavailable } from "../../config/config";
+import ContentModal from "../ContentModal/ContentModal";
 import './SingleContent.css';
+
+
 
 const SingleContent = ({
   id,
@@ -12,7 +15,7 @@ const SingleContent = ({
 }) => {
 
   return( 
-    <div className="media">
+    <ContentModal media_type={media_type} id={id}>
       <Badge badgeContent= {vote_average} color={vote_average>6?'primary':'secondary'} />
       <img className='poster' src={poster? `${img_300}/${poster}`: unavailable } alt={title}/>
 
@@ -21,7 +24,7 @@ const SingleContent = ({
         {media_type === "tv" ? "TV Series" : "Movie"}
         <span className="subTitle">{date}</span>
       </span>
-    </div>
+    </ContentModal>
   );
 };
 
